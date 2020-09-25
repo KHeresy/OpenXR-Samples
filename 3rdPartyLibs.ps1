d54076706900f6e8d620063701a146cc3d3ac66a
Heresy <kheresy@gmail.com>
2020-09-25 10:46:32 +0800

    Update ExtLib path

--- 3rdPartyLibs.ps1
+++ 3rdPartyLibs.ps1
@@ -1,2 +1,8 @@
-Invoke-Webrequest -Uri https://github.com/KhronosGroup/OpenXR-SDK/releases/download/release-1.0.9/openxr_loader_windows-1.0.9.zip -OutFile .\openxr_loader.zip
-Expand-Archive -Path .\openxr_loader.zip -DestinationPath .\
+New-Variable -Name "ExtPath"	-Value ".\ExtLib\"
+
+# OpenXR Runtime
+Invoke-Webrequest -Uri https://github.com/KhronosGroup/OpenXR-SDK/releases/download/release-1.0.11/openxr_loader_windows-1.0.11.zip -OutFile $ExtPath\openxr_loader.zip
+Expand-Archive -Path $ExtPath\openxr_loader.zip -DestinationPath $ExtPath\tmp
+Copy-Item -Recurse -Path $ExtPath\tmp\openxr_loader_windows\include\openxr -Destination $ExtPath\include\
+Copy-Item -Recurse -Path $ExtPath\tmp\openxr_loader_windows\x64\lib -Destination $ExtPath\lib
+Copy-Item -Recurse -Path $ExtPath\tmp\openxr_loader_windows\x64\bin -Destination $ExtPath\dll
