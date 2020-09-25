b4ce95d1371d3ad02e3e9f917f7b3d105029acbd
Heresy <kheresy@gmail.com>
2020-09-25 10:57:22 +0800

    Add Basic glut + OpenGL sample code

--- 3rdPartyLibs.ps1
+++ 3rdPartyLibs.ps1
@@ -6,3 +6,10 @@ Expand-Archive -Path $ExtPath\openxr_loader.zip -DestinationPath $ExtPath\tmp
 Copy-Item -Recurse -Path $ExtPath\tmp\openxr_loader_windows\include\openxr -Destination $ExtPath\include\
 Copy-Item -Recurse -Path $ExtPath\tmp\openxr_loader_windows\x64\lib -Destination $ExtPath\lib
 Copy-Item -Recurse -Path $ExtPath\tmp\openxr_loader_windows\x64\bin -Destination $ExtPath\dll
+
+# freeglut
+Invoke-Webrequest -Uri https://www.transmissionzero.co.uk/files/software/development/GLUT/freeglut-MSVC.zip -OutFile $ExtPath\freeglut.zip
+Expand-Archive -Path $ExtPath\freeglut.zip -DestinationPath $ExtPath\tmp
+Copy-Item -Recurse -Path $ExtPath\tmp\freeglut\include\GL -Destination $ExtPath\include\
+Copy-Item -Path $ExtPath\tmp\freeglut\lib\x64\freeglut.lib -Destination $ExtPath\lib
+Copy-Item -Path $ExtPath\tmp\freeglut\bin\x64\freeglut.dll -Destination $ExtPath\dll
