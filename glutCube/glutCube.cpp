241905a61d54ea2984778c0f708d4b1f42286752
Heresy <kheresy@gmail.com>
2020-09-25 11:19:50 +0800

    Add glew (not used yet)

--- glutCube/glutCube.cpp
+++ glutCube/glutCube.cpp
@@ -1,16 +1,14 @@
-// From https://www.opengl.org/archives/resources/code/samples/glut_examples/examples/cube.c
+// The OpenGL sample is from https://www.opengl.org/archives/resources/code/samples/glut_examples/examples/cube.c
 
-/* Copyright (c) Mark J. Kilgard, 1997. */
-
-/* This program is freely distributable without licensing fees
-	-and is provided without guarantee or warrantee expressed or
-	implied. This program is -not- in the public domain. */
-
-/* This program was requested by Patrick Earl; hopefully someone else
-	will write the equivalent Direct3D immediate mode program. */
-
+// OpenGL related Headers
+#include <GL/glew.h>
 #include <GL/glut.h>
 
+// link lib
+#pragma comment(lib,"freeglut.lib")
+#pragma comment(lib,"glew32.lib") 
+
+#pragma region OpenGL render related data
 GLfloat light_diffuse[] = { 1.0, 0.0, 0.0, 1.0 };  /* Red diffuse light. */
 GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };  /* Infinite light location. */
 GLfloat n[6][3] = {  /* Normals for the 6 faces of a cube. */
@@ -20,6 +18,7 @@ GLint faces[6][4] = {  /* Vertex indices for the 6 faces of a cube. */
 	{0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
 	{4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3} };
 GLfloat v[8][3];  /* Will be filled in with X,Y,Z vertexes. */
+#pragma endregion
 
 void drawBox(void)
 {
@@ -81,6 +80,7 @@ int main(int argc, char** argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutCreateWindow("red 3D lighted cube");
+	glewInit();
 	glutDisplayFunc(display);
 	init();
 	glutMainLoop();
